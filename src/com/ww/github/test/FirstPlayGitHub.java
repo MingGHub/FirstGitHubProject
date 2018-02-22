bb259d223c9dace6d0afdaa3ee473644713d0a5a
MingGHub <36350024+MingGHub@users.noreply.github.com>
2018-02-22 15:59:36 +0800

    大胜靠德吧

--- src/com/ww/github/test/FirstPlayGitHub.java
+++ src/com/ww/github/test/FirstPlayGitHub.java
@@ -5,5 +5,6 @@ public class FirstPlayGitHub {
 	public static void main(String[] args) {
 		System.out.println("hello GitHub!!!");
 		System.out.println("successfully");
+		System.out.println("successfully1111111111111111111");
 	}
 }
