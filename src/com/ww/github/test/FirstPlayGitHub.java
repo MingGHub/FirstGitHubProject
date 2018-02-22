8cffe7868877d5153ee14be2da85f00fa449cf3c
MingGHub <36350024+MingGHub@users.noreply.github.com>
2018-02-22 16:29:41 +0800

    update 02

--- src/com/ww/github/test/FirstPlayGitHub.java
+++ src/com/ww/github/test/FirstPlayGitHub.java
@@ -7,6 +7,7 @@ public class FirstPlayGitHub {
 		System.out.println("successfully");
 		System.out.println("successfully1111111111111111111");
 		System.out.println("successfully2222222222222222222");
+		System.out.println("successfully3333333333333333333");
 
 	}
 }
